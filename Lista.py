8b543047b03a880a38dc423db4290423a0affb03
kristianps <kristian64480416@alunos.sc.senac.br>
2026-05-06 19:35:27 -0300

    ListaPart1

---
+++ Lista.py
@@ -0,0 +1,8 @@
+produtos = ("MacBook", "Celular", "Computador", "Teclado", "Mouse")
+preços = ("1500", "1000", "2000", "500", "250")
+
+print(produtos[0]) # imprime "MacBook"
+print(produtos[1]) # imprime "Celular"
+print(produtos[2]) # imprime "Cmputador"
+print(produtos[3]) # imprime "Teclado"
+print(produtos[4]) # imprime "Mouse"
