0d6d13de185b64f1a449b4f102c261fb027a402a
kristianps <kristianpereira759@gmail.com>
2026-05-20 21:53:29 -0300

    part4

---
+++ cidades.py
@@ -0,0 +1,8 @@
+cidades = ["Blumenau" , "Gaspar" , "Indaial" , "Brusque"]
+
+for cidade in cidades:
+    if cidade [0] == "B":
+        print(cidade)
+        continue
+    else:
+        print(cidade)
